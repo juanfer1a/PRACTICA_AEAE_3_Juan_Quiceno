60c31d6fbbe694382e3ec31ab52b1d084bc297ed
Juanfer1a <jundi1a@gmail.com>
2022-05-12 15:27:01 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRACTICA_AEAE_3_Juan_Quiceno.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
