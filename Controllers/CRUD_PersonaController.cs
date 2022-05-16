951d3380374f48e274645112665664ccd88e0e22
Juanfer1a <jundi1a@gmail.com>
2022-05-16 16:50:05 -0500

    Prueba

---
+++ Controllers/CRUD_PersonaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRACTICA_AEAE_3_Juan_Quiceno.Controllers
+{
+    public class CRUD_PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRACTICA_AEAE_3_Juan_Quiceno.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
