2f8125901ecca8d1a8067199afcc0201065929b3
Juanfer1a <jundi1a@gmail.com>
2022-05-21 18:06:18 -0500

    again

--- Controllers/TBLclienteController.cs
+++ Controllers/TBLclienteController.cs
@@ -5,19 +5,18 @@ using PRACTICA_AEAE_3_Juan_Quiceno.Models;
 
 namespace PRACTICA_AEAE_3_Juan_Quiceno.Controllers
 {
-    public class TBLclienteController : Controller
+    public class TblClienteController : Controller
     {
         private readonly FACTURASContext _context;
 
-        public TBLclienteController(FACTURASContext context)
+        public TblClienteController(FACTURASContext context)
         {
             _context = context;
         }
 
-        public async Task<ActionResult> Cliente()
-        {
-            return View(await _context.Tblclientes.ToListAsync());
-        }
-       
+        public async Task<ActionResult> Index()        
+         => View(await _context.Tblclientes.ToListAsync());
+        
+
     }
 }
--- Controllers/TBLproductoController.cs
+++ Controllers/TBLproductoController.cs
@@ -1,83 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PRACTICA_AEAE_3_Juan_Quiceno.Controllers
 {
-    public class TBLproductoController : Controller
+    public class TblProductoController : Controller
     {
-        // GET: TBLproductoController
-        public ActionResult Producto()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        // GET: TBLproductoController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: TBLproductoController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TBLproductoController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TBLproductoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TBLproductoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TBLproductoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TBLproductoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
